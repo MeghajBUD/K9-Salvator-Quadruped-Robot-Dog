6f43a492d0f8ce0d76caa797939296af6ed51921
AndrewMessiha <amessiha@purdue.edu>
2026-02-13 10:12:59 -0500

    Add files via upload

---
+++ simulations/mcb_pmsm_foc_hall_AK45_10_KV75_data.m
@@ -0,0 +1,73 @@
+% Model         :   PMSM Field Oriented Control
+% Description   :   Set Parameters for PMSM Field Oriented Control
+% File name     :   mcb_pmsm_foc_hall_f28379d_data.m
+
+% Copyright 2020-2024 The MathWorks, Inc.
+
+%% Set PWM Switching frequency
+PWM_frequency 	= 20e3;             %Hz     // converter s/w freq
+T_pwm           = 1/PWM_frequency;  %s      // PWM switching time period
+
+%% Set Sample Times
+Ts          	= T_pwm;            %sec    // Sample time step for controller
+Ts_simulink     = T_pwm/2;          %sec    // Simulation time step for model simulation
+Ts_motor        = T_pwm/2;          %Sec    // Simulation sample time
+Ts_inverter     = T_pwm/2;          %sec    // Simulation time step for average value inverter
+Ts_speed        = 10*Ts;            %Sec    // Sample time for speed controller
+
+%% Set data type for controller & code-gen
+% dataType = fixdt(1,32,17);        % Fixed point code-generation
+dataType = 'single';                % Floating point code-generation
+
+%% System Parameters
+% Motor parameters
+pmsm = getMotorParameters('AK45_10_KV75')
+pmsm.PositionOffset = 0.17;         % Per-Unit position offset
+
+%% Target & Inverter Parameters
+target = mcb.getProcessorParameters('F28379D',PWM_frequency);
+target.comport = '<Select a port...>';
+% target.comport = 'COM3';       % Uncomment and update the appropriate serial port
+
+inverter = mcb.getInverterParameters('BoostXL-DRV8305');
+
+% Enable automatic calibration of ADC offset for current measurement
+inverter.ADCOffsetCalibEnable = 1;  % Enable: 1, Disable: 0
+
+% If automatic ADC offset calibration is disabled, uncomment and update the 
+% offset values below manually
+% inverter.CtSensAOffset = 2295;      % ADC Offset for phase current A 
+% inverter.CtSensBOffset = 2286;      % ADC Offset for phase current B
+
+% Update inverter.ISenseMax based for the chosen motor and target
+inverter = mcb.updateInverterParameters(pmsm,inverter,target);
+
+% Max and min ADC counts for current sense offsets
+inverter.CtSensOffsetMax = 2500;    % Maximum permitted ADC counts for current sense offset
+inverter.CtSensOffsetMin = 1500;    % Minimum permitted ADC counts for current sense offset
+
+%% Derive Characteristics
+pmsm.N_base = mcb.getMotorBaseSpeed(pmsm,inverter); %rpm // Base speed of motor at given Vdc
+% mcb_getCharacteristics(pmsm,inverter);
+
+%% PU System details // Set base values for pu conversion
+
+PU_System = mcb.getPUSystemParameters(pmsm,inverter);
+
+%% Controller design // Get ballpark values!
+
+PI_params = mcb.getPIControllerParameters(pmsm,inverter,PU_System,T_pwm,Ts,Ts_speed);
+
+%Updating delays for simulation
+PI_params.delay_Currents    = int32(Ts/Ts_simulink);
+PI_params.delay_Position    = int32(Ts/Ts_simulink);
+PI_params.delay_Speed       = int32(Ts_speed/Ts_simulink);
+PI_params.delay_Speed1       = (PI_params.delay_IIR + 0.5*Ts)/Ts_speed;
+
+% mcb.getMotorControlAnalysis(pmsm,inverter,PU_System,PI_params,Ts,Ts_speed); 
+
+%% Displaying model variables
+disp(pmsm);
+disp(inverter);
+disp(target);
+disp(PU_System);
